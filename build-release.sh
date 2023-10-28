88038eadcc4ccf37bc989a8a05a8488e840b4925
Gabriel Nordeborn <gabbe.nord@gmail.com>
2023-10-28 11:52:20 +0200

    0.2.0

--- build-release.sh
+++ build-release.sh
@@ -3,6 +3,7 @@ echo "Building PPX locally...";
 esy;
 cp _build/default/bin/RescriptEmbedLang.exe release/ppx-macos-arm64;
 echo "Copying assets...";
-cp -f README.md postinstall.js release/;
+cp -rf src release/;
+cp -f README.md postinstall.js bsconfig.json release/;
 cp -f package-release.json release/package.json;
 echo "Done!";
