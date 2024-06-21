d0233ddc1651fcef2788498e3d8375dc69a2c9df
Gabriel Nordeborn <gabbe.nord@gmail.com>
2024-06-21 17:57:40 +0200

    bsconfig.json -> rescript.json

--- build-release.sh
+++ build-release.sh
@@ -1,6 +1,6 @@
 mkdir -p release;
 echo "Copying assets...";
 cp -rf src release/;
-cp -f README.md postinstall.js bsconfig.json release/;
+cp -f README.md postinstall.js rescript.json release/;
 ./copyPackageJson.js $INPUT_TAG_NAME
 echo "Done!";
